79883f3a986a8c182c2c0851b52e432577350845
Maurits Lamers <maurits@weidestraat.nl>
2010-08-26 15:20:16 +0200

    Adding addFiniteObserver to have an one time observer

--- frameworks/runtime/mixins/observable.js
+++ frameworks/runtime/mixins/observable.js
@@ -766,6 +766,29 @@ SC.Observable = {
     return this;
   },
   
+  addFiniteObserver: function(key, target, method, context){
+     
+     // normalize.  if a function is passed to target, make it the method.
+     if (method === undefined) {
+       method = target; target = this ;
+     }
+     if (!target) target = this ;
+
+     if (typeof method === "string") method = target[method] ;
+     if (!method) throw "You must pass a method to addObserver()" ;
+
+     var me = this; // replace by context?
+     var f = function() { 
+        var val = me.get(key);
+        var ret = method.call(target,val);
+        //console.log("addFiniteObserver function called: val = " + val + " and ret = " + ret);
+        if(ret){
+           me.removeObserver(key, me, f); 
+        }
+     };
+     this.addObserver(key, this, f);
+  },
+  
   /**
     Returns YES if the object currently has observers registered for a 
     particular key.  You can use this method to potentially defer performing
